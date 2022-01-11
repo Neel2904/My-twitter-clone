fc3e064724b81951a32436a943ad8783f64e6a50
Neel2904 <mgorasiya1974@gmail.com>
2022-01-11 12:23:57 +0530

    sidebar finished

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/index.js",
+    "./components/Sidebar.js",
+    "./components/SidebarLink.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
