5c09b14fe9e21d37843842af0ebb16d7a40b499c
Neel2904 <mgorasiya1974@gmail.com>
2022-01-21 17:22:09 +0530

    final commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "059cf9064bbed9c7"; }
+/******/ 		__webpack_require__.h = function() { return "6619fe720c08d941"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Feed.js
+++ components/Feed.js
@@ -35,8 +35,6 @@ function Feed() {
     [db]
   );
 
-  console.log(posts);
-  console.log(posts.length);
   return (
     <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9d] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
